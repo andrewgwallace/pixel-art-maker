17d495b08c38ebb4467ab4728a517ef6f2e4770a
andrewgwallace <andrew.g.wallace@gmail.com>
2018-03-03 21:14:03 -0500

    Add script.js, style.css, and build out basic HTML.

---
+++ script.js
@@ -0,0 +1,3 @@
+// Create a function that creates the divs in the HTML rather than manually creating them.
+
+//Use event.target to act on the div the user has clicked on, and call the mouse actions
